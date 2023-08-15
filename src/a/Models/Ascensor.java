fe462a6468b9639411927344d3f3c4b7ad3c914f
SebasVba <sebastianbarquerocr@gmail.com>
2023-08-15 14:36:14 -0600

    Creacion Proyecto y más
    Se crea el proyeycto junto algunas clases principales

---
+++ src/a/Models/Ascensor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.Models;
+
+/**
+ *
+ * @author sebas
+ */
+public class Ascensor {
+    private int pisoActual;
+    private int pisoSiguiente;
+    private int pesoMax;
+    private boolean abajo;
+    private boolean arriba;
+    
+}
---
+++ src/a/Models/Sistema.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.Models;
+
+/**
+ *
+ * @author sebas
+ */
+public class Sistema {
+    
+    public boolean validarPesoMax(){
+        return false;
+    }
+    
+    public boolean validarPlanta(){
+        return false;
+    }
+    
+    public void moverAscensor(int siguientePlanta){
+        
+    }
+}
